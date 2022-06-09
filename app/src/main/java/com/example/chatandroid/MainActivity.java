cd8eadb1805826091f198b8aa87bf1f6ff64dc9b
ShaharShaki <shaharshaki217@gmail.com>
2022-06-09 19:12:48 +0300

    f

--- app/src/main/java/com/example/chatandroid/MainActivity.java
+++ app/src/main/java/com/example/chatandroid/MainActivity.java
@@ -36,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
                     startActivity(in);
                 }
             }
+            //dd
             popupMessage();
         });
 
