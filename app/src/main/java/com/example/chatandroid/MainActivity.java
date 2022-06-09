b993fdbc5bf4e49d390750fb807ef928e6945ce6
ShaharShaki <shaharshaki217@gmail.com>
2022-06-09 19:07:00 +0300

    Initial commit

---
+++ app/src/main/java/com/example/chatandroid/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.chatandroid;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    static List<String> usernamesList;
+    static List<String> passwordsList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        usernamesList = new ArrayList<String>();
+        passwordsList = new ArrayList<String>();
+
+        String username = findViewById(R.id.editTextTextPersonName).toString();
+        String password = findViewById(R.id.editTextTextPassword).toString();
+
+        Button btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(v -> {
+            Intent in = new Intent(this, ChatActivity.class);
+            for (int i = 0; i < usernamesList.size(); i++) {
+                if (usernamesList.get(i).equals(username) && passwordsList.get(i).equals(password)) {
+                    startActivity(in);
+                }
+            }
+            popupMessage();
+        });
+
+        Button btnRegister = findViewById(R.id.btnRegister);
+        btnRegister.setOnClickListener(v -> {
+            Intent i2 = new Intent(this, RegisterActivity.class);
+            startActivity(i2);
+        });
+
+    }
+
+    public void popupMessage(){
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setMessage("Not a valid input, please try again.");
+        alertDialogBuilder.setTitle("Wrong input");
+        alertDialogBuilder.setNegativeButton("ok", new DialogInterface.OnClickListener(){
+
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Log.d("internet","Ok btn pressed");
+                // add these two lines, if you wish to close the app:
+                finishAffinity();
+                System.exit(0);
+            }
+        });
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+    }
+}
---
+++ app/src/main/java/com/example/chatandroid/RegisterActivity.java
@@ -0,0 +1,86 @@
+package com.example.chatandroid;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private final int GALLERY_REQ_CODE = 1000;
+    ImageView imgGallery;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        EditText username = findViewById(R.id.editTextTextPersonName);
+        EditText password = findViewById(R.id.editTextTextPassword);
+        EditText password2 = findViewById(R.id.editTextTextPasswordValidation);
+
+        ListView lvUsers = findViewById(R.id.lvUsers);
+//        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, )
+
+        ImageView imgGallery = findViewById(R.id.imgGallery);
+        Button btnGallery = findViewById(R.id.btnImg);
+
+        Button Submit = findViewById(R.id.btnSubmit);
+        Submit.setOnClickListener(v -> {
+            if ((password.getText().toString().equals(password2.getText().toString())) &&
+                    !(MainActivity.usernamesList.contains(username.getText().toString()))) {
+                MainActivity.usernamesList.add(username.getText().toString());
+                MainActivity.usernamesList.add(password.getText().toString());
+                Intent i2 = new Intent(this, MainActivity.class);
+                startActivity(i2);
+            }
+            else {
+                popupMessage();
+//                onCreate(savedInstanceState);
+            }
+
+        });
+
+//        btnGallery.setOnClickListener(v -> {
+//            Intent iGallery = new Intent(Intent.ACTION_PICK);
+//            iGallery.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+//            startActivityForResult(iGallery, GALLERY_REQ_CODE);
+//        });
+    }
+
+//    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//        if (resultCode == RESULT_OK) {
+//            if(requestCode == GALLERY_REQ_CODE) {
+//                imgGallery.setImageURI(data.getData());
+//            }
+//        }
+//    }
+
+
+    public void popupMessage(){
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setMessage("Not a valid input, please try again.");
+        alertDialogBuilder.setTitle("Wrong input");
+        alertDialogBuilder.setNegativeButton("ok", new DialogInterface.OnClickListener(){
+
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Log.d("internet","Ok btn pressed");
+                // add these two lines, if you wish to close the app:
+                finishAffinity();
+                System.exit(0);
+            }
+        });
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+    }
+}
